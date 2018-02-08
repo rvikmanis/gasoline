1e53d96112aa6f6fc41cdca3cc536c717e9315c3
Rudolfs Vikmanis <rudolfs.vikmanis@gmail.com>
2018-02-08 04:31:08 +0200

    Fix `examples/counter`, so it works with latest changes to the library

--- examples/counter/src/index.tsx
+++ examples/counter/src/index.tsx
@@ -1,323 +1,14 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import * as Rx from "rxjs";
-import * as gasoline from "gasoline";
+import { Model, combineModels, Observable, Store, connect, UpdateContext, ModelInterface, InputAction, ActionLike } from "gasoline";
 import './index.css';
 
-const counter = new gasoline.Model({
+const counter = new Model({
 
   actionCreators: {
     increment() {
       return { type: "INCREMENT" }
-    },
-    inc10() {
-      return { type: "INC_10" }
     }
-  },
-
-  process(action$) {
-    const action = counter.actionCreators.increment()
-    return action$.ofType("INC_10").concatMapTo([
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-
-      action, action, action, action, action, action,
-      action, action, action, action,
-    ]).observeOn(Rx.Scheduler.async)
   },
 
   update(state: number = 0, context) {
@@ -331,7 +22,7 @@ const counter = new gasoline.Model({
 
 });
 
-const autoIncrement = new gasoline.Model({
+const autoIncrement = new Model({
 
   actionCreators: {
     toggle() {
@@ -353,17 +44,17 @@ const autoIncrement = new gasoline.Model({
 
       // When the store starts, and
       // each time TOGGLE_AUTO_INCREMENT is dispatched,
-      .ofType(gasoline.Store.START, "TOGGLE_AUTO_INCREMENT")
+      .ofType(Store.START, "TOGGLE_AUTO_INCREMENT")
       .switchMap(() => {
 
         if (model.state) {
           // if auto increment is enabled, emit INCREMENT every second
           const action = counter.actionCreators.increment()
-          return Rx.Observable.interval(1000).mapTo(action)
+          return Observable.interval(1000).map(() => action)
         }
 
         // otherwise emit nothing.
-        return Rx.Observable.empty()
+        return Observable.empty()
 
         // Note: switchMap takes care of subscribing to the latest stream
         // and unsubscribing from previous streams.
@@ -371,46 +62,77 @@ const autoIncrement = new gasoline.Model({
   }
 });
 
-const rootModel = gasoline.combineModels({
+type SetValueAction = {
+  type: "SET_VALUE";
+  payload: string;
+}
+
+type TextFieldDependencies = {
+  counter: typeof counter
+}
+
+const textField = new Model({
+  dependencies: {
+    counter: counter
+  },
+  update(state: string = "", context: UpdateContext<TextFieldDependencies, SetValueAction & ActionLike>) {
+    if (context.action.type === "SET_VALUE") {
+      state = context.action.payload
+    }
+
+    state = context.dependencies.counter % 2 === 0
+      ? state.toUpperCase()
+      : state.toLowerCase()
+
+    return state
+  },
+  process(action$, model) {
+    return action$.ofType("SET_VALUE").map(() => {
+      return counter.actionCreators.increment()
+    })
+  },
+  actionCreators: {
+    setValue(e: any): SetValueAction & InputAction {
+      return { type: "SET_VALUE", payload: e.target.value, target: textField }
+    }
+  }
+})
+
+const rootModel = combineModels({
   counter,
-  autoIncrement
+  autoIncrement,
+  textField
 });
 
-const store = new gasoline.Store(rootModel);
+const store = new Store(rootModel);
 
-const createContainer = gasoline.connect(
-  rootModel.state$,
-  {
-    increment: counter.actions.increment,
-    toggleAutoIncrement: autoIncrement.actions.toggle,
-    inc10: counter.actions.inc10
-  }
-)
+const CounterApp = connect(rootModel)(() => {
 
-const CounterApp = createContainer(props => {
   const incrementBtn = (
-    <button disabled={props.autoIncrement}
-      onClick={props.increment}>
+    <button disabled={autoIncrement.state}
+            onClick={counter.actions.increment}>
       Increment
-        </button>
+    </button>
   )
 
   const toggleBtn = (
-    <button onClick={props.toggleAutoIncrement}>
-      {props.autoIncrement ? "Disable" : "Enable"} auto inc.
-        </button>
+    <button onClick={autoIncrement.actions.toggle}>
+      {autoIncrement.state ? "Disable" : "Enable"} auto inc.
+    </button>
   )
 
-  const inc10Btn = (
-    <button onClick={props.inc10}>inc. 10</button>
+  const textInput = (
+    <input onChange={textField.actions.setValue} value={textField.state} />
   )
 
   return (
     <div>
-      <div>{props.counter}</div>
-      <div>{incrementBtn} {toggleBtn} {inc10Btn}</div>
+      <div>{counter.state} test</div>
+      <div>{incrementBtn} {toggleBtn}</div>
+      <div>{textInput}</div>
     </div>
   )
+
 })
 
 store.ready(() => {
@@ -418,10 +140,6 @@ store.ready(() => {
     <CounterApp />,
     document.querySelector("#app")
   )
-
-  // store.action$.withLatestFrom(store.model.state$).subscribe(([action, state]) => {
-  //   console.log(action, state)
-  // })
 })
 
 store.start()
