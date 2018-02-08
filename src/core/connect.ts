a53d76883b8c897002e58422c91809aeceb2a6cb
Rudolfs Vikmanis <rudolfs.vikmanis@gmail.com>
2018-02-08 04:29:06 +0200

    Change how `connect` works
    `connect` now takes a single argument - the model.

--- src/core/connect.ts
+++ src/core/connect.ts
@@ -1,63 +1,23 @@
 import { ComponentType, ComponentClass, Component, createElement } from "react"
-import { Scheduler, Subscription, Observable } from "rxjs";
 import shallowEquals from "shallow-equals";
+import { ModelInterface } from "../interfaces";
 
-function connect<T extends object>(
-    obs$: Observable<T>
-): (component: ComponentType<T>) => ComponentClass<{}>;
-
-function connect<R extends object, T, P extends object>(
-    obs$: Observable<T>,
-    mapper: ((state: T, ownProps: P) => R)
-): (component: ComponentType<R>) => ComponentClass<P>;
-
-function connect<T extends object, S extends object>(
-    obs$: Observable<T>,
-    staticProps: S
-): (component: ComponentType<T & S>) => ComponentClass<{}>;
-
-function connect<R extends object, T, P extends object, S extends object>(
-    obs$: Observable<T>,
-    mapper: ((state: T, ownProps: P) => R),
-    staticProps: S
-): (component: ComponentType<R & S>) => ComponentClass<P>;
-
-function connect(
-    obs$: Observable<any>,
-    mapper?: any,
-    staticProps?: any
-): any {
-    if (typeof mapper === "object" && mapper !== null) {
-        staticProps = mapper
-        mapper = undefined
-    }
-
-    if (staticProps == null) {
-        staticProps = {}
-    }
-
-    if (mapper == null) {
-        mapper = (t: any, p: any) => t
-    }
-
-    return (component: any) => {
+function connect<M extends ModelInterface, S = M["state"], A = M["actions"]>(
+    model: M
+): (component: ComponentType<{ state: S, actions: A }>) => ComponentClass<{}> {
+    return (component: ComponentType<{ state: S, actions: A }>) => {
         return class extends Component {
             static displayName = component.displayName;
 
-            subscription: Subscription;
-            initialStateSubscription: Subscription;
-            sourceState: any;
+            subscription: ZenObservable.Subscription;
+            sourceState: S;
 
             componentWillMount() {
-                const initialState$ = obs$.takeUntil(Observable.of(null, Scheduler.async))
-                this.initialStateSubscription = initialState$.subscribe(sourceState => {
-                    this.sourceState = sourceState
-                })
+                this.sourceState = model.state
             }
 
             componentDidMount() {
-                this.initialStateSubscription.unsubscribe()
-                this.subscription = obs$.subscribe(sourceState => {
+                this.subscription = model.state$.subscribe(sourceState => {
                     if (!shallowEquals(sourceState, this.sourceState)) {
                         this.sourceState = sourceState
                         this.forceUpdate()
@@ -76,7 +36,7 @@ function connect(
             render() {
                 return createElement(
                     component as any,
-                    { ...staticProps, ...mapper(this.sourceState, this.props) }
+                    { state: this.sourceState, actions: model.actions }
                 )
             }
         }
